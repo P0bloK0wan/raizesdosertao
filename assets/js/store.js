d09914b5a1943f142827dc51289e040683a1b651
Pablo Kawan <pablokawan58@gmail.com>
2026-09-25 08:38:06 -0300

    feat: salva tipo e link opcional de álbuns de mídia

--- assets/js/store.js
+++ assets/js/store.js
@@ -612,8 +612,8 @@ export function watchMidia(cb) {
     cb(snap.docs.map((d) => ({ id: d.id, ...d.data() })));
   });
 }
-export function addPastaMidia(nome, fotos = []) {
-  return addDoc(collection(db, "midia"), { nome, fotos, criadoEm: serverTimestamp() });
+export function addPastaMidia(nome, fotos = [], link = "") {
+  return addDoc(collection(db, "midia"), { nome, fotos, link, tipo: link ? "drive" : "fotos", criadoEm: serverTimestamp() });
 }
 export function adicionarFotosMidia(pastaId, fotos) {
   return updateDoc(doc(db, "midia", pastaId), { fotos });
